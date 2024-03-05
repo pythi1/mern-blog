e1e0cb10b0531dd08ccc25300fd8d0501da34850
Pramod <134283019+pramodjangra@users.noreply.github.com>
2024-03-05 15:03:43 +0530

    created routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Singin from './pages/Singin';
+import Singup from './pages/Singup';
+import Projects from './pages/Projects';
 
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-3xl font-bold' >Vite + React</h1>
-    </>
+    <BrowserRouter>
+      <Routes> 
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<Singin />} />
+        <Route path="/signup" element={<Singup />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
