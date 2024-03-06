c1ab674beda8e654c23fcd02123302117a69c42b
Pramod <134283019+pramodjangra@users.noreply.github.com>
2024-03-06 14:02:02 +0530

    user data model

---
+++ api/models/user.model.js
@@ -0,0 +1,20 @@
+import { Mongoose } from "mongoose";
+
+
+const userSchema = new Mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+}, { timestamps: true } );
+
+const User = Mongoose.model("User", userSchema );
+
+export default User;
