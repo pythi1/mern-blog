db36a5f2e1a6dd6c4fcc9c9ce0249b0303ea4119
Pramod <134283019+pramodjangra@users.noreply.github.com>
2024-03-13 16:45:46 +0530

    done with the signin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -74,11 +74,11 @@ export const signin = async (req, res, next) => {
 
         const token = jwt.sign({ id: validUser._id, }, process.env.JWT_SECRET);
 
-        // const { password : pass, ...rest} = validUser._docs;
+        const { password: pass , ...rest} = validUser._doc;
 
         res.status(200).cookie("access_token", token, {
             httpOnly: true
-        }).json(validUser);
+        }).json(rest);
 
     } catch (error) {
         // console.log(error);
