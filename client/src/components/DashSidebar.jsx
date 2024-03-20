56f9d3ddf1c6e4f6919af1d4226cf1d8778859df
Pramod <134283019+pramodjangra@users.noreply.github.com>
2024-03-20 17:38:43 +0530

    dashboard profile

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,41 @@
+import React, { Profiler } from 'react';
+import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import { Sidebar } from "flowbite-react";
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from "react-router-dom"
+
+function DashSidebar() {
+
+    const location = useLocation();
+    const [tab, setTab] = useState("");
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get("tab");
+        console.log(tabFromUrl);
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
+    }, [location.search])
+    return (
+
+        <Sidebar className='w-full md:w-56' >
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Link to="/dashboard?tab=profile">
+                        <Sidebar.Item active={tab === "profile"} icon={HiUser} label={"User"} labelColor="dark">
+                            Profile
+                        </Sidebar.Item>
+                    </Link>
+                    <Sidebar.Item icon={HiArrowSmRight} labelColor="dark" clasname="cursor-pointer" >
+                        Sign Out
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
+
+        </Sidebar>
+
+    )
+}
+
+export default DashSidebar;
