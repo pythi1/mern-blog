0edec71a45dfae291a6ea7f08077ca255601518c
Pramod <134283019+pramodjangra@users.noreply.github.com>
2024-03-22 17:40:01 +0530

    complete profile update functionality in ui

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,19 +2,19 @@ import User from "../models/user.model.js";
 import { errorhandler } from "../utils/error.js";
 
 export const test = (req, res) => {
-    res.json({message: " api is running"});
+    res.json({ message: " api is running" });
 };
 
 
 export const Updateuser = async (req, res, next) => {
     console.log(req.user);
 
-    if(req.user.id !== req.params.userId){
+    if (req.user.id !== req.params.userId) {
         return next(errorhandler(403, "you are not allowed to update this user."));
     }
 
-    if(req.body.password){
-        if(req.body.password.length < 6 ){
+    if (req.body.password) {
+        if (req.body.password.length < 6) {
             return next(errorhandler(400, "length of the passwprd must be more than 6 char."));
 
         }
@@ -23,37 +23,38 @@ export const Updateuser = async (req, res, next) => {
 
     }
 
-    if(req.body.username){
-        if(req.body.username.length < 7 || req.body.username.length > 20){
+    if (req.body.username) {
+        if (req.body.username.length < 7 || req.body.username.length > 20) {
             return next(errorhandler(400, "username must be between 7 to 20 char"));
         }
 
-        if(req.body.username.includes(' ')){
+        if (req.body.username.includes(' ')) {
             return next(errorhandler(400, "username can't have space"));
         }
-        
-        if(req.body.username !== req.body.username.toLowerCase()){
+
+        if (req.body.username !== req.body.username.toLowerCase()) {
             return next(errorhandler(400, "username must be in lowercase. "))
         }
 
-        if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
+        if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
             return next(errorhandler(400, 'username can only be contains letter and numbers.'))
         }
-
-        try {
-            const Updateduser = await User.findByIdAndUpdate(req.params.userId, {
-                $set: {
-                    username: req.body.username,
-                    email: req.body.email,
-                    profilePicture: req.body.profilePicture,
-                    password: req.body.password,
-                    
-                }, 
-            }, {new: true});
-            const {password, ...rest } = Updateduser._doc;
-            res.status(200).json(rest);
-        } catch (error) {
-            next(error);
-        }
     }
+
+    try {
+        const Updateduser = await User.findByIdAndUpdate(req.params.userId, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                profilePicture: req.body.profilePicture,
+                password: req.body.password,
+
+            },
+        }, { new: true });
+        const { password, ...rest } = Updateduser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+
 }
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,6 +5,8 @@ import { getDownloadURL, getStorage, uploadBytesResumable, ref } from "firebase/
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { updateStart, updateSuccess, updateFailure } from '../redux/user/Userslice.js';
+import { useDispatch} from "react-redux"
 
 function DashProfile() {
 
@@ -12,8 +14,14 @@ function DashProfile() {
   const [imageFile, setimageFile] = useState(null);
   const [imageUrl, setimageUrl] = useState(null);
   const filePickerRef = useRef();
-  const [imagefileUploadingProgress, setimagefileUploadingProgress] = useState(null);
-  const [imagefileuploaderror, setimagefileuploaderror] = useState(null);
+  const [imagefileUploadingProgress, setimagefileUploadingProgress] = useState(null); // ye dikhane ke liye ki kitni file uplaod ho gyi hai 
+  const [imagefileuploaderror, setimagefileuploaderror] = useState(null); // error jab image file uplpaod kr rahe ho.
+  const [imageFileUploading, setimageFileUploading] = useState(false); // jab file uploading ho rahi ho tab
+  const [updateUserSuccess, setUpdateUserSuccess ] = useState(null);
+  const [updateuserError, setupdateuserError] = useState(null);
+  const [formdata, setformdata] = useState({});
+
+  const dispatch = useDispatch();
 
   // console.log(imagefileUploadingProgress, imagefileuploaderror);
 
@@ -33,9 +41,14 @@ function DashProfile() {
     }
   }, [imageFile]);
 
+
+
   const uploadImage = async () => {
     console.log("uploading image....");
 
+
+
+    setimageFileUploading(true);
     setimagefileuploaderror(null);
 
     const storage = getStorage(app);
@@ -54,14 +67,15 @@ function DashProfile() {
         setimagefileUploadingProgress(null);
         setimageFile(null);
         setimageUrl(null);
+        setimageFileUploading(false);
 
       },
-
-      
 
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
           setimageUrl(downloadUrl);
+          setformdata({...formdata, profilePicture : downloadUrl});
+          setimageFileUploading(false);
         });
       }
 
@@ -69,10 +83,59 @@ function DashProfile() {
 
   };
 
+  const handleInputeChange = (e) => {
+    setformdata({...formdata, [e.target.id] : e.target.value})
+  };
+
+  const handleInputSubmit = async (e) => {
+    e.preventDefault();
+    setupdateuserError(null);
+    setUpdateUserSuccess(null);
+    if(Object.keys(formdata).length === 0){
+      setupdateuserError("no changes Made");
+      return;
+    }
+
+    if(imageFileUploading){
+      setupdateuserError("please wait for  image to finish uploading");
+      return;
+    }
+
+    try {
+
+      dispatch(updateStart());
+      const userid = currentuser._id
+      const res = await fetch(`/api/user/update/${userid}`, {
+        method: "PUT",
+        headers: {"content-Type" : "application/json"},
+        body: JSON.stringify(formdata),
+      });
+      
+      const data = await res.json();
+
+      if(!res.ok){
+        dispatch(updateFailure(data.message));
+        setupdateuserError(data.message)
+      }
+      else{
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("Update Successfully...")
+      }
+      
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+      setupdateuserError(error.message);
+    }
+
+}
+
+  console.log(formdata);
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full' >
       <h1 className='my-7 text-center font-semibold text-3xl' >Profile</h1>
-      <form className='flex flex-col gap-4' >
+
+      <form onSubmit={handleInputSubmit} className='flex flex-col gap-4' >
         <input type='file' accept='image/*' onChange={handleimagechange} ref={filePickerRef} hidden />
 
         <div className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={() => filePickerRef.current.click()} >
@@ -110,18 +173,23 @@ function DashProfile() {
 
 
 
-        <TextInput type='text' id="username" placeholder='Username' defaultValue={currentuser.username} />
-        <TextInput type='email' id="email" placeholder='email' defaultValue={currentuser.email} />
-        <TextInput type='password' id="password" placeholder='password' />
+        <TextInput type='text' id="username" placeholder='Username' defaultValue={currentuser.username} onChange={handleInputeChange} />
+        <TextInput type='email' id="email" placeholder='email' defaultValue={currentuser.email} onChange={handleInputeChange} />
+        <TextInput type='password' id="password" placeholder='password' onChange={handleInputeChange} />
 
         <Button type='submit' gradientDuoTone="purpleToBlue" outline >Update</Button>
+
+      </form> 
 
         <div className='text-red-500 flex justify-between mt-5 ' >
           <span className='cursor-pointer' >Delete Acount</span>
           <span className='cursor-pointer' >Delete Acount</span>
         </div>
 
-      </form>
+        {updateUserSuccess && (<Alert color="success" className='mt-5'>{updateUserSuccess}</Alert> )}
+
+        {updateuserError && (<Alert color="failure" className='mt-5'>{updateuserError}</Alert> )}
+
     </div>
   )
 }
