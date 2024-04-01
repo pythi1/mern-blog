e307cf0095ab3bc4958f3ea25903e81ea04f66c6
Pramod <134283019+pramodjangra@users.noreply.github.com>
2024-04-01 12:31:48 +0530

    added call to action button in post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Button, Spinner } from 'flowbite-react';
+import CallToAction from '../components/CallToAction';
 
 export default function PostPage() {
 
@@ -79,6 +80,10 @@ export default function PostPage() {
                 dangerouslySetInnerHTML={{ __html: post && post.content }}
             >
 
+            </div>
+
+            <div className='max-w-4xl mx-auto w-full' >
+                <CallToAction />
             </div>
 
         </main>
